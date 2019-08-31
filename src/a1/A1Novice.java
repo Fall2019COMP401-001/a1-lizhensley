fb71522c51c83d5faacc4a953939dfae4aa38997
Elizabeth Hensley <elizabethannehensley@gmail.com>
2019-08-31 09:51:39 -0400

    A1Novice attempt 1

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -10,5 +10,28 @@ public class A1Novice {
 
 		// Your code follows here.
 		
+		int numCustomers = scan.nextInt();
+		
+		for (int i = 0; i < numCustomers; i++) {
+			
+			String firstName = scan.next();
+			String lastName = scan.next();
+			int numItems = scan.nextInt();
+			
+			Double total = 0.0;
+			
+			for (int j = 0; j < numItems; j++) {
+				
+				int quantity = scan.nextInt();
+				String itemName = scan.next();
+				Double price = scan.nextDouble();
+				total += price;
+				
+			}
+			
+			System.out.println(firstName.charAt(0) + ". " + lastName + ": " + total);
+			
+		}
+		
 	}
 }
